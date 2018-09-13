d42b4218955f8e5bd8e25b05eb3366a7ba5b66e3
John <john@Johns-MacBook-Pro.local>
2018-09-13 08:33:54 -0500

    Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -4,25 +4,52 @@
 // 	console.log(i)
 // }
 
+
 //Get Even
 
 // for (i = 0; i <= 200; i += 2) {
 // 	console.log(i)
 // }
 
+
 //Excited Kitten
 
-const catPhrases = ['...human...why you taking pictures of me?..', '..the catnip made me do it...', '..why does the red dot always get away...'];
-const randomPhrase = catPhrases[Math.floor(catPhrases.length * Math.random())];
+// const catPhrases = ['...human...why you taking pictures of me?..', '..the catnip made me do it...', '..why does the red dot always get away...'];
+// const randomPhrase = catPhrases[Math.floor(catPhrases.length * Math.random())];
 
-
-for (i = 0; i < 20; i++) {
-	console.log('Love me, pet me! HSSSSSS!')
-	if (i % 2 === 0) {
-		console.log(randomPhrase);
-	}
-}
+// for (i = 0; i < 20; i++) {
+// 	console.log('Love me, pet me! HSSSSSS!')
+// 	if (i % 2 === 0) {
+// 		console.log(randomPhrase);
+// 	}
+// }
 
 //right now this just returns the SAME randomized phrase each time, for all the evens... 
 
 
+//Fizz Buzz
+
+
+// for (i = 1; i < 100; i++) {
+// 		if (i % 3 === 0) {
+// 			console.log('Fizz')
+// 		} if (i % 5 === 0) {
+// 			console.log('Buzz')
+// 		} if (i % 3 === 0 || i % 5 === 0) {
+// 			console.log('Fizzbuzz')
+// 		}
+// }
+
+// ^^ classic original vv displays number alongside Fizz/Buzz/Fizzbuzz - built so I could double check the numbers. 
+
+// for (i = 1; i < 100; i++) {
+// 		if (i % 3 === 0) {
+// 			console.log('Fizz ' + i)
+// 		} if (i % 5 === 0) {
+// 			console.log('Buzz ' + i)
+// 		} if (i % 3 === 0 || i % 5 === 0) {
+// 			console.log('Fizzbuzz ' + i)
+// 		}
+// }
+
+//
